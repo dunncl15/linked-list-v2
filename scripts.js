450e3e2cab0bfe87661dc77290a4cd83dd594b38
Charles Dunn <dunnc1551@gmail.com>
2016-12-23 11:36:38 -0700

    Add functionality to send new bookmark to local storage. Create send to storage function. Create remove from storage function.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
 
 //Event Listeners
+
+//Enter button
 $('.enter-btn').on('click', function(e) {
   e.preventDefault();
-  getUserInput();
+  addNewBookmark();
 })
 
+//Functions
 function Bookmark(title, url) {
   this.title = title;
   this.url = url;
   this.id = Date.now();
 }
 
-function addBookmark(Bookmark) {
+function newBookmark(Bookmark) {
   $('.bookmark-section').append(`<div id="${Bookmark.id}" class="bookmark">
     <p class="website-title" contenteditable>${Bookmark.title}</p>
     <a class="website-url" href="http://${Bookmark.url}" target="_blank" contenteditable>${Bookmark.url}</a>
@@ -20,9 +23,19 @@ function addBookmark(Bookmark) {
   </div>`);
 }
 
-function getUserInput() {
+function addNewBookmark() {
   var websiteTitle = $('.web-title').val();
   var websiteURL = $('.web-url').val();
   var userBookmark = new Bookmark(websiteTitle, websiteURL);
-  addBookmark(userBookmark);
+  newBookmark(userBookmark);
+  sendToStorage(userBookmark.id, userBookmark);
+}
+
+function sendToStorage(id, object) {
+  var stringifiedBookmark = JSON.stringify(object);
+  localStorage.setItem(id, stringifiedBookmark);
+}
+
+function removeFromStorage(id) {
+  var parsedBookmark = JSON.parse(localStorage.getItem(id));
 }
