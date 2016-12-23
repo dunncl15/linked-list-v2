f1b57ef460884e594efd4f7ae1f4014171bf55ff
Charles Dunn <dunnc1551@gmail.com>
2016-12-23 11:12:18 -0700

    Add functionality to append bookmark to the page. Add event listener to enter button to append bookmarks.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
+
+//Event Listeners
+$('.enter-btn').on('click', function(e) {
+  e.preventDefault();
+  getUserInput();
+})
+
 function Bookmark(title, url) {
   this.title = title;
   this.url = url;
   this.id = Date.now();
 }
 
+function addBookmark(Bookmark) {
+  $('.bookmark-section').append(`<div id="${Bookmark.id}" class="bookmark">
+    <p class="website-title" contenteditable>${Bookmark.title}</p>
+    <a class="website-url" href="http://${Bookmark.url}" target="_blank" contenteditable>${Bookmark.url}</a>
+    <button class="read-btn">Read</button>
+    <button class="delete-btn">Delete</button>
+  </div>`);
+}
+
 function getUserInput() {
   var websiteTitle = $('.web-title').val();
   var websiteURL = $('.web-url').val();
-  var bookmark = new Bookmark(websiteTitle, websiteURL)
-  addBookmark(bookmark);
+  var userBookmark = new Bookmark(websiteTitle, websiteURL);
+  addBookmark(userBookmark);
 }
